e6bf5c522fab588d9962a57731a9939c5df681c8
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2025-04-14 16:07:26 +0400

    large message

--- app/utils/send_large_message.py
+++ app/utils/send_large_message.py
@@ -4,5 +4,16 @@ MAX_MESSAGE_LENGTH = 4000
 
 
 async def send_large_message(bot: Bot, chat_id: int, text: str, **kwargs):
-    for i in range(0, len(text), MAX_MESSAGE_LENGTH):
-        await bot.send_message(chat_id, text[i:i + MAX_MESSAGE_LENGTH], **kwargs)
+    lines = text.split("\n")
+    chunk = ""
+
+    for line in lines:
+        # Проверка, не превысит ли добавление строки лимит
+        if len(chunk) + len(line) + 1 < MAX_MESSAGE_LENGTH:
+            chunk += line + "\n"
+        else:
+            await bot.send_message(chat_id, chunk.strip(), **kwargs)
+            chunk = line + "\n"
+
+    if chunk:
+        await bot.send_message(chat_id, chunk.strip(), **kwargs)
